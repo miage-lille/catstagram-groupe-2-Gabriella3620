9c6a7733228fd23233781571db0c8abe642c9ed1
Ishimwe Gabriella Divine <gabriella.ishimwe.etu@b13p3.fil.univ-lille.fr>
2025-02-04 17:11:56 +0100

    debut exo1

--- src/reducer.ts
+++ src/reducer.ts
@@ -2,17 +2,21 @@ import { Loop, liftState } from 'redux-loop';
 import { compose } from 'redux';
 import { Actions } from './types/actions.type';
 
-export type State = unknown; // TODO : Update this type !
+export type State = {
+  counter: number,
+}
 
-export const defaultState = {}; // TODO : Update this value !
+export const defaultState: State = {
+  counter: 3
+}
 
 export const reducer = (state: State | undefined, action: Actions): State | Loop<State> => {
   if (!state) return defaultState; // mandatory by redux
   switch (action.type) {
     case 'INCREMENT':
-      throw 'Not Implemented';
+      return { ...state, counter: state.counter + 1};
     case 'DECREMENT':
-      throw 'Not Implemented';
+      return { ...state, counter: state.counter - 1};
     case 'SELECT_PICTURE':
       throw 'Not Implemented';
     case 'CLOSE_MODAL':
